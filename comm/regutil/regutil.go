d4a91cd21431550cd1c10407096e5cdff5fc8675
lingcoder <lingcoder@gmail.com>
2022-06-15 17:54:11 +0800

    Syn libary util

--- comm/regutil/regutil.go
+++ comm/regutil/regutil.go
@@ -1,11 +1,11 @@
 package regutil
 
 import (
+	"github.com/ethereum/go-ethereum/common"
 	"regexp"
 )
 
 const (
-	RegETHAddress        = "^0[xX][0-9a-zA-Z]{40}$"
 	RegPositiveIntNumber = "^[1-9]\\d*"
 )
 
@@ -13,8 +13,7 @@ func IsETHAddress(address string) bool {
 	if address == "" {
 		return false
 	}
-	flag, err := regexp.MatchString(RegETHAddress, address)
-	return err == nil && flag
+	return common.IsHexAddress(address)
 }
 
 func IsPositiveIntNumber(s string) bool {
